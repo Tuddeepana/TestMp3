9c2eb6ac86c42216db36f526179af472a3e14401
Tuddeepana <tauddeepana@gmail.com>
2024-01-15 22:28:34 +0530

    new update comment

--- view/Mainview.java
+++ view/Mainview.java
@@ -14,84 +14,60 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-
 /**
  * Mainview
  */
- class Mp3 extends JFrame{
+class Mp3 extends JFrame {
 
     JLabel namePlayer;
-    JButton pusueButton; 
+    JButton pusueButton;
     JButton playButton;
 
-    Mp3(){
+    Mp3() {
         setSize(780, 320);
         setTitle("My Test Mp3");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-
-        //Set name of the player
+        // Set name of the player
         namePlayer = new JLabel();
         namePlayer.setText("My Mp3");
         Font boldAppleFont = new Font("Helvetica Neue", Font.BOLD, 15);
         namePlayer.setFont(boldAppleFont);
-        
 
         JPanel namePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         namePanel.add(namePlayer);
 
-
-        //set pause Button
-        JPanel jPanel = new JPanel(new GridLayout(2,2));
+        // set pause Button
+        JPanel jPanel = new JPanel(new GridLayout(2, 2));
 
         pusueButton = new JButton("Pause");
-        pusueButton.setFont(new Font("",1,12));
+        pusueButton.setFont(new Font("", 1, 12));
         pusueButton.setForeground(Color.WHITE);
-        
 
         pusueButton.setBackground(Color.BLACK);
 
-
-        //Play Button
+        // Play Button
         playButton = new JButton("Play");
-        playButton.setFont(new Font("",1,12));
+        playButton.setFont(new Font("", 1, 12));
         playButton.setForeground(Color.WHITE);
         playButton.setBackground(Color.RED);
 
-
-
-
-
-
-
-
-        
-       
-        //setup Layout for pause button
+        // setup Layout for pause button
+        //Right side
         JPanel setBottJPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         setBottJPanel.add(playButton);
         setBottJPanel.add(pusueButton);
         jPanel.add(setBottJPanel);
         jPanel.add(setBottJPanel);
-        
 
-
-       
-
-
-
-
-
-        add("North",namePanel);
-        add("South",jPanel);
+        add("North", namePanel);
+        add("South", jPanel);
         setVisible(true);
 
-
-
     }
-    
+
 }
 
 /**
